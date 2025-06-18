79837d056dc41ecd78b4024c58ead1a1f14af178
bbc12200 <71333470+bbc12200@users.noreply.github.com>
2025-06-18 00:00:12 -0700

    Project1

---
+++ p1_process.h
@@ -0,0 +1,19 @@
+#ifndef __P1_PROCESS
+#define __P1_PROCESS
+
+#include <vector>
+
+// Student struct
+struct student {
+  unsigned long id;
+  double grade;
+
+  student(unsigned long id, double grade) {
+    this->id = id;
+    this->grade = grade;
+  }
+};
+
+void create_processes_and_sort(std::vector<std::string>, int, int);
+
+#endif
